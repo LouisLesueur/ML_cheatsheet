897c470cfec4e789c44e75e0a8ad694e154c519f
Louis <louis.lesueur@gmx.fr>
2020-10-26 16:47:56 +0100

    bootstrap template

--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 rm plots/*
-pandoc docs/index.md -s --filter pandoc-plot --katex -o index.html -c style.css --toc
+pandoc docs/index.md -s  -o index.html -c style.css --toc --template pandoc-bootstrap/template.html --include-in-header pandoc-bootstrap/header.html --include-before-body pandoc-bootstrap/navbar.html --include-after-body pandoc-bootstrap/footer.html --filter pandoc-plot --katex
